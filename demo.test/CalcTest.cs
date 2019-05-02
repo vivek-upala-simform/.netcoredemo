338ee334d8ed8bdb669ad33fabf9cb60d71007b1
Vivek Upala <Vivek.U@simformsolutions.com>
2019-05-02 15:43:03 +0530

    Added Test Project and modified travis.yml file

---
+++ demo.test/CalcTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using netcoredemo.Service;
+namespace demo.test
+{
+    public class CalcTest
+    {
+        [Fact]
+        public void Sum()
+        {
+            int expected = 5;
+            int actual = MathService.Add(2, 3);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ netcoredemo/Service/MathService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace netcoredemo.Service
+{
+    public class MathService
+    {
+        public static int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public static double Division(int a,int b)
+        {
+            return a/b;
+        }
+    }
+}
